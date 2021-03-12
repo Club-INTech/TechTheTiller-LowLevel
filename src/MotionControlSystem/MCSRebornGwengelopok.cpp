6c2d85cefc19e62694f16293b667db24f6e49474
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2021-03-13 00:54:35 +0100

    PID for speed 60 montlhery

--- src/MotionControlSystem/MCSRebornGwengelopok.cpp
+++ src/MotionControlSystem/MCSRebornGwengelopok.cpp
@@ -16,9 +16,9 @@ MCS::MCS(): leftMotor(Side::LEFT), rightMotor(Side::RIGHT)  {
   
 #if defined(MAIN)
 
-    leftSpeedPID.setTunings(1, 0, 0, 0); //0.5   0.000755   21.5
+    leftSpeedPID.setTunings(0.87, 1e-6, 0, 0); //0.5   0.000755   21.5
     leftSpeedPID.enableAWU(false);
-    rightSpeedPID.setTunings(0.85, 0.000001, 0, 0); //0.85 0.000755 0
+    rightSpeedPID.setTunings(0.86, 1.6*1e-6, 0, 0); //0.85 0.000755 0
     rightSpeedPID.enableAWU(false);
 
     translationPID.setTunings(2,0,0,0);
